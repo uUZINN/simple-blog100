8f13979c5e23e98c66a85fae8b5b5848496f7414
L-jy16 <89904583+uUZINN@users.noreply.github.com>
2023-12-11 18:08:13 +0900

    2023.12.11

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/scss/style.scss';
+import App from './App';
+
+import store from './Reducer/store';
+import { Provider } from 'react-redux';
+
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ server/config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://dbwls8929:why9038@cluster0.vw5jixs.mongodb.net/?retryWrites=true&w=majority",
+    access_key: "6DKwufBufItsMMIZtQs0",
+    secret_key: "2kDpYaaMoQ6h1zxwwPJeivPh4p5E67tJ29zx8b7Y"
+}
